e2c86469463548d55243f64d0c55ffe8d786e52f
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-11-02 17:55:40 +0000

    displays date only

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,24 +1,44 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var currentDayEl = $('#currentDay');
+var pastTimeEl =$('.row time-block past');
+var presentTimeEl = $('.row time-block present');
+varFutureTimeEl = $('.row time-block future ');
+var saveBtnEl =$('.saveBtn');
+
+
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
+
+    // Save the user input in local storage using the id as the key
+    saveBtnEl.on("click", function () {
+    var userInput = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlockId, userInput);
+    });
+      
+        // Apply the past, present, or future class to each time block
+         var currentHour = dayjs().hour();
+        $(".time-block").each(function () {
+            var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+
+            if (timeBlockHour < currentHour) {
+            $(this).addClass(pastTimeEl);
+            } else if (timeBlockHour === currentHour) {
+            $(this).addClass(presentTimeEl);
+            } else {
+            $(this).addClass(futureTimeEl);
+            }
+        });
+
+        // Get any user input saved in local storage and set the values of the textarea elements
+       $(".time-block").each(function () {
+        var timeBlockId = $(this).attr("id");
+        var savedUserInput = localStorage.getItem(timeBlockId);
+    
+        if (savedUserInput) {
+          $(this).find(".description").val(savedUserInput);
+        }
+      });
+    
+      var currentDate = dayjs().format("dddd, MMMM D");
+      currentDayEl.text(currentDate);
+    });
+  
--- README.md
+++ README.md
@@ -37,7 +37,7 @@ THEN the saved events persist
 The following animation demonstrates the application functionality:
 
 <!-- @TODO: create ticket to review/update image) -->
-![A user clicks on slots on the color-coded calendar and edits the events.](./assets/images/05-third-party-apis-homework-demo.gif)
+![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/Image/05-third-party-apis-homework-demo.gif)
 
 ## Grading Requirements
 
