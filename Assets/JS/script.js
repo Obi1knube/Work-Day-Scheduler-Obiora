8e62796350224c08c4a318bdd712d00b70704733
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-11-03 04:15:14 +0000

    saves to local storage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,44 +1,44 @@
 var currentDayEl = $('#currentDay');
-var pastTimeEl =$('.row time-block past');
+var pastTimeEl = $('.row time-block past');
 var presentTimeEl = $('.row time-block present');
-varFutureTimeEl = $('.row time-block future ');
-var saveBtnEl =$('.saveBtn');
+var futureTimeEl = $('.row time-block future');
+var saveBtnEl = $('.saveBtn');
 
-
+function handleSaveBtn(event) {
+    // Prevent the default behavior
+    event.preventDefault();
+  }
 
 $(function () {
-
-    // Save the user input in local storage using the id as the key
-    saveBtnEl.on("click", function () {
+  // Save the user input in local storage using the id as the key
+  saveBtnEl.on("click", function () {
     var userInput = $(this).siblings(".description").val();
+    var timeBlockId = $(this).closest(".time-block").attr("id"); // Get the id of the parent time-block element
     localStorage.setItem(timeBlockId, userInput);
-    });
-      
-        // Apply the past, present, or future class to each time block
-         var currentHour = dayjs().hour();
-        $(".time-block").each(function () {
-            var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+  });
 
-            if (timeBlockHour < currentHour) {
-            $(this).addClass(pastTimeEl);
-            } else if (timeBlockHour === currentHour) {
-            $(this).addClass(presentTimeEl);
-            } else {
-            $(this).addClass(futureTimeEl);
-            }
-        });
+  // Apply the past, present, or future class to each time block
+  var currentHour = dayjs().hour();
+  $(".time-block").each(function () {
+    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (timeBlockHour < currentHour) {
+      $(this).addClass(pastTimeEl);
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass(presentTimeEl);
+    } else {
+      $(this).addClass(futureTimeEl);
+    }
+  });
 
-        // Get any user input saved in local storage and set the values of the textarea elements
-       $(".time-block").each(function () {
-        var timeBlockId = $(this).attr("id");
-        var savedUserInput = localStorage.getItem(timeBlockId);
-    
-        if (savedUserInput) {
-          $(this).find(".description").val(savedUserInput);
-        }
-      });
-    
-      var currentDate = dayjs().format("dddd, MMMM D");
-      currentDayEl.text(currentDate);
-    });
-  
+  // Get any user input saved in local storage and set the values of the textarea elements
+  $(".time-block").each(function () {
+    var timeBlockId = $(this).attr("id");
+    var savedUserInput = localStorage.getItem(timeBlockId);
+    if (savedUserInput) {
+      $(this).find(".description").val(savedUserInput);
+    }
+  });
+
+  var currentDate = dayjs().format("dddd, MMMM D");
+  currentDayEl.text(currentDate);
+});
