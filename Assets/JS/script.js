7230aa0a0128926456c7806fd53feea2f9ef1d04
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-11-04 13:10:07 +0000

    fully working

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,11 +4,6 @@ var presentTimeEl = $('.row time-block present');
 var futureTimeEl = $('.row time-block future');
 var saveBtnEl = $('.saveBtn');
 
-function handleSaveBtn(event) {
-    // Prevent the default behavior
-    event.preventDefault();
-  }
-
 $(function () {
   // Save the user input in local storage using the id as the key
   saveBtnEl.on("click", function () {
@@ -18,18 +13,30 @@ $(function () {
   });
 
   // Apply the past, present, or future class to each time block
-  var currentHour = dayjs().hour();
-  $(".time-block").each(function () {
-    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
-    if (timeBlockHour < currentHour) {
-      $(this).addClass(pastTimeEl);
-    } else if (timeBlockHour === currentHour) {
-      $(this).addClass(presentTimeEl);
-    } else {
-      $(this).addClass(futureTimeEl);
-    }
-  });
+  function hourUpdate(){
+    var currentHour = dayjs().hour();
+    $(".time-block").each(function () {
+      var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+       console.log(timeBlockHour);
+       console.log(currentHour);
+  
+      if (timeBlockHour < currentHour) {
+      
+        $(this).addClass('past');
+      } else if (timeBlockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  
 
+  }
+  hourUpdate();
+  setInterval(hourUpdate,15000);
   // Get any user input saved in local storage and set the values of the textarea elements
   $(".time-block").each(function () {
     var timeBlockId = $(this).attr("id");
@@ -39,6 +46,22 @@ $(function () {
     }
   });
 
-  var currentDate = dayjs().format("dddd, MMMM D");
+
+//equivalent iteration in javaScript
+
+//To achieve the same functionality in pure JavaScript, you can use the document.querySelectorAll() method to select elements with the class "time-block" and then loop through them. Here's the JavaScript code that replicates the provided jQuery code:
+
+// document.querySelectorAll(".time-block").forEach(function (element) {
+//     var timeBlockId = element.getAttribute("id");
+//     var savedUserInput = localStorage.getItem(timeBlockId);
+//     if (savedUserInput) {
+//         var descriptionElement = element.querySelector(".description");
+//         if (descriptionElement) {
+//             descriptionElement.value = savedUserInput;
+//         }
+//     }
+// });
+
+  var currentDate = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
   currentDayEl.text(currentDate);
 });
